fef787c3c87a8022021fbfd3336ff32eff86458e
b0927208 <155470119+b0927208@users.noreply.github.com>
2024-03-17 15:24:05 +0800

    Update main2.c

--- hw2/main2.c
+++ hw2/main2.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
+
 int main() {
-    FILE *inputFile,*outputFile;
+    FILE *inputFile, *outputFile;
     char ch;
-    int linenum=1;
     inputFile = fopen("main2.c", "r");
     outputFile = fopen("main2.txt", "w");
-    printf("0%d ",linenum);
+    int linenum=1;
+    fprintf(outputFile,"0%d ",linenum);
     linenum++;
     while ((ch = fgetc(inputFile)) != EOF) {
         if(ch!='\n'){
-        fputc(ch, outputFile);
-        printf("%c",ch);
+            fputc(ch, outputFile);
         }
         else{
-            if(linenum<10)printf("0%d ",linenum);
-            else printf("%d ",linenum);
+            if(linenum<10)fprintf(outputFile,"0%d ",linenum);
+            else fprintf(outputFile,"%d ",linenum);
             linenum++;
         }
     }
