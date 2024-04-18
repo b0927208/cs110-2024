e9361bf42cc3246f4771a38f68e07a13f7ea4e7e
b0927208 <155470119+b0927208@users.noreply.github.com>
2024-04-18 15:11:03 +0800

    Update main.cpp

--- EX4/main.cpp
+++ EX4/main.cpp
@@ -3,18 +3,26 @@
 using namespace std;
 
 class Car {
+    protected:
+    string m_DriveMode;
+    private:
+    int m_MaxSeating;
+    int m_price;
+    void m_UpdatePrice(int base=500000){
+        m_price=m_MaxSeating*base;
+    }
     public:
     string m_brand;
     string m_model;
     int m_year;
-    int m_MaxSeating;
-    int m_price;
+
     Car(string x,string y,int z,int s){
         m_brand=x;
-        m_brand=y;
+        m_model=y;
         m_year=z;
         m_MaxSeating=s;
-        m_price=m_MaxSeating*500000;
+        m_UpdatePrice();
+        m_DriveMode="No-wheel";
     }
     int get_MaxSeatint(){
         return m_MaxSeating;
@@ -22,14 +30,51 @@ class Car {
     int get_Price(){
         return m_price;
     }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
     
+};
+class BMW_Car : public Car{
+
+    public:
+    BMW_Car(string y,int z,int s):Car("BMW",y,z,s){
+        cout<<"Constructing BMW_Car\n";
+        m_DriveMode="Rear-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+class AUDI_Car : public Car{
+
+    public:
+    AUDI_Car(string y,int z,int s):Car("Audi",y,z,s){
+        cout<<"Constructing AUDI_Car\n";
+        m_DriveMode="Front-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
+};
+class BENZ_Car : public Car{
+
+    public:
+    BENZ_Car(string y,int z,int s):Car("Benz",y,z,s){
+        cout<<"Constructing BENZ_Car\n";
+        m_DriveMode="Front-wheel";
+    }
+    string get_DriveMode(){
+        return m_DriveMode;
+    }
 };
     
 int main(){
-    Car car_1("BMW","X5",2023,6);
-    Car car_2("Audi","A1",2021,2);
-    cout<<car_1.m_brand<<" Max Seating = "<<car_1.get_MaxSeatint()<<endl;
-    cout<<car_2.m_brand<<" Max Seating = "<<car_2.get_MaxSeatint()<<endl;
-    cout<<car_1.m_brand<<" Price = "<<car_1.get_Price()<<endl;
-    cout<<car_2.m_brand<<" Price = "<<car_2.get_Price()<<endl;
+    BMW_Car car_1("X5",2023,6);
+    cout<<car_1.m_brand<<" : Max Seating = "<<car_1.get_DriveMode()<<endl;
+    AUDI_Car car_2("A1",2021,2);
+    cout<<car_2.m_brand<<" : Max Seating = "<<car_2.get_DriveMode()<<endl;
+    BENZ_Car car_3("C-class",2022,5);
+    cout<<car_3.m_brand<<" : Max Seating = "<<car_3.get_DriveMode()<<endl;
+
 }
